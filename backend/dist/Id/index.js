a117e8e6cd30a78dd378af7f494111fe2f87774a
sahilkamboj3 <kambojsahil327@gmail.com>
2020-11-22 23:03:47 -0800

    dist removed

--- backend/dist/Id/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cuid = require("cuid");
-const Id = Object.freeze({
-    makeId: cuid,
-    isValidId: cuid.isCuid
-});
-exports.default = Id;
-//# sourceMappingURL=index.js.map
