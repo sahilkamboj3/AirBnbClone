f9ec46b6ff19b3def574ff692d2e063cde762e8d
sahilkamboj3 <kambojsahil327@gmail.com>
2020-11-23 19:25:21 -0800

    re

--- backend/src/user/makeEncryptedPassword.ts
+++
@@ -1,7 +0,0 @@
-export default function makeEncryption ({ bcrypt, content, saltRounds = 10 }) {
-    const hash = bcrypt.hash(content, saltRounds);
-
-    return Object.freeze({
-        password: hash
-    })
-}
