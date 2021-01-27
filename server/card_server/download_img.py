5fe51fa8f891e63a99f12fc2507fb50272e30c75
sahilkamboj3 <kambojsahil327@gmail.com>
2021-01-27 02:21:49 -0800

    flask 8080 server made for downloading

--- server/card_server/download_img.py
+++ server/card_server/download_img.py
@@ -1,5 +1,7 @@
 from PIL import Image
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import string
+import random
 
 app = Flask(__name__)
 
@@ -9,13 +11,29 @@ def hello_world():
     return 'Hello World'
 
 
-@app.route('/download', methods=['POST', 'GET'])
-def download():
+@app.route('/download/<filename>', methods=['POST', 'GET'])
+def download(filename):
     data = request.get_data()
+    ascii_ = string.ascii_letters
+    new_filename = filename + '_'
 
-    # with open()
+    for i in range(3):
+        num = random.randint(0, 9)
+        letter = random.choice(ascii_)
+        new_filename += str(num) + letter
 
-    return 'download'
+    image = Image.new('RGB', (200, 200))
+    image.save('/media/' + new_filename + '.jpg')
+
+    with open('/media/' + new_filename + '.jpg', 'wb') as f:
+        f.write(data)
+
+    response = {
+        'image_name': new_filename + '.jpg',
+        'status': 200
+    }
+
+    return jsonify(response)
 
 
 if __name__ == '__main__':
